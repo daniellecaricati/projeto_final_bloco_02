40e09d684ef75929e0cd5101b9b9b5889c96b769
Danielle <daniellecaricati1@gmail.com>
2026-06-09 15:10:50 -0300

    Atualização Controller

--- src/main/java/com/generation/farmacia/controller/ProdutoController.java
+++ src/main/java/com/generation/farmacia/controller/ProdutoController.java
@@ -68,10 +68,15 @@ public class ProdutoController {
 	
 	@PutMapping
 	public ResponseEntity<Produto> put (@Valid @RequestBody Produto produto){
-		return produtoRepository.findById(produto.getId())
-				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
-				.body(produtoRepository.save(produto)))
-				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+		
+		if (produtoRepository.existsById(produto.getId())) {
+
+			if (categoriaRepository.existsById(produto.getCategoria().getId()))
+				return ResponseEntity.status(HttpStatus.OK).body(produtoRepository.save(produto));
+
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Categoria não existe!", null);
+	}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
 	
 	@DeleteMapping("/{id}")
